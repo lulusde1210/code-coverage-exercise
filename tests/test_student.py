8b69273bebcb015df990f6ed2f970e023fcf8125
lulu <lulusun1210@gmail.com>
2023-04-05 10:04:34 -0500

    fix the bugs and updated tests to have 100% code coverage

--- tests/test_student.py
+++ tests/test_student.py
@@ -1,5 +1,6 @@
 from student.student import Student, get_student_with_more_classes
 
+
 def test_init():
     name = "Ada Lovelace"
     level = "sophomore"
@@ -11,6 +12,18 @@ def test_init():
     assert ada.level == level
     assert ada.courses == courses
 
+
+def test_init_empty_courses():
+    name = "Ada Lovelace"
+    level = "sophomore"
+
+    ada = Student(name, level)
+
+    assert ada.name == name
+    assert ada.level == level
+    assert ada.courses == []
+
+
 def test_add_class():
     new_class = 'Intro to Feminism'
     charles = Student("Charles Babbage", "senior", ["mechanical engineering"])
@@ -19,10 +32,12 @@ def test_add_class():
     assert len(charles.courses) == 2
     assert new_class in charles.courses
 
+
 def test_get_num_classes():
     george = Student("George Byron", "senior", ["advanced poetry"])
 
     assert george.get_num_classes() == 1
+
 
 def test_summary():
     anne = Student(
@@ -33,7 +48,20 @@ def test_summary():
 
     assert anne.summary() == "Anne Byron is a senior enrolled in 2 classes"
 
-def test_get_student_with_more_classes():
+
+def test_get_student_with_more_classes_return_student_a():
+    charles = Student("Charles Babbage", "senior", [
+                      "mechanical engineering", "Math", "PE"])
+    ada = Student(
+        "Ada Lovelace",
+        "sophomore",
+        ["mathematics", "foundations of computing"]
+    )
+
+    assert get_student_with_more_classes(charles, ada) == charles
+
+
+def test_get_student_with_more_classes_return_student_b():
     charles = Student("Charles Babbage", "senior", ["mechanical engineering"])
     ada = Student(
         "Ada Lovelace",
@@ -41,4 +69,4 @@ def test_get_student_with_more_classes():
         ["mathematics", "foundations of computing"]
     )
 
-    # TODO: write assertions
+    assert get_student_with_more_classes(charles, ada) == ada
